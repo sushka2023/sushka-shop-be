f144f62cab37845ca51cbe74f389846501701068
Vladislav <109065019+TimVladislav13010@users.noreply.github.com>
2023-06-23 00:13:47 +0300

    add base src

---
+++ main.py
@@ -0,0 +1,44 @@
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+from src.database.db import get_db
+# from src.routes import users, auth, images
+
+app = FastAPI()
+
+
+@app.get("/", description='Main page')
+def root():
+    """
+    Main page definition
+
+    :return: dict: health status
+
+    """
+    return {"message": "Welcome to the FastAPI Store!"}
+
+
+# @app.get("/api/healthchecker")
+# def healthchecker(db: Session = Depends(get_db)):
+#     """
+#     Health Checker
+#
+#     :param db: database session
+#     :return: dict: health status
+#     """
+#     try:
+#         result = db.execute(text("SELECT 1")).fetchone()
+#         if result is None:
+#             raise HTTPException(status_code=500, detail="Database is not configured correctly")
+#         return {"message": "Welcome to FastAPI!"}
+#     except Exception:
+#         raise HTTPException(status_code=500, detail="Error connecting to the database")
+
+
+# app.include_router(users.router, prefix='/api')
+# app.include_router(auth.router, prefix='/api')
+# app.include_router(images.router, prefix='/api')
+
--- src/database/db.py
+++ src/database/db.py
@@ -1,2 +1,22 @@
+from src.conf.config import settings
+from fastapi import HTTPException, status
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 
 
+SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Dependency
+def get_db():
+    db = DBSession()
+    try:
+        yield db
+    except SQLAlchemyError as err_sql:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err_sql))
+    finally:
+        db.close()
