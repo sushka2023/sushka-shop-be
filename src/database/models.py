52371499eff94b46fcd5290bcbab562d488d5f52
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-07-01 20:41:49 +0300

    models_favorite

--- src/database/models.py
+++ src/database/models.py
@@ -15,6 +15,14 @@ class Role(enum.Enum):
     user: str = 'user'
 
 
+class PaymentType(enum.Enum):
+    """
+    Payment type.
+    """
+    cash_on_delivery_np: str = 'cash_on_delivery_np'
+    liqpay: str = 'liqpay'
+
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
@@ -30,6 +38,9 @@ class User(Base):
     refresh_token = Column(String(255), nullable=True)
     is_deleted = Column(Boolean, default=False)
     is_blocked = Column(Boolean, default=False)
+    basket = relationship("Basket", uselist=False, back_populates="user")
+    orders = relationship("Order", back_populates="user")
+    posts = relationship("Post", uselist=False, back_populates="user")
 
 
 class BlacklistToken(Base):
@@ -43,10 +54,11 @@ class Product(Base):
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
     name = Column(String(150), unique=False, nullable=False)
-    description = Column(String(500), unique=False, nullable=False)
+    description = Column(String(150), unique=False, nullable=False)
     product_category = relationship("ProductCategory", uselist=False, back_populates="product")
-    reviews_id = Column('reviews_id', ForeignKey('reviews.id', ondelete='CASCADE'), default=None)
-    rating_id = Column('rating_id', ForeignKey('rating.id', ondelete='CASCADE'), default=None)
+    prices = relationship("Price", back_populates="product")
+    images = relationship("Image", back_populates="product")
+    reviews = relationship("Review", back_populates="product")
     promotional = Column(Boolean, default=False)
     new_product = Column(Boolean, default=False)
     is_deleted = Column(Boolean, default=False)
@@ -56,15 +68,17 @@ class Image(Base):
     __tablename__ = 'images'
     id = Column(Integer, primary_key=True)
     product_id = Column('product_id', ForeignKey('product.id', ondelete='CASCADE'), default=None)
+    product = relationship("Product", back_populates="images")
     image_url = Column(String(255), unique=True, nullable=False)
     created_at = Column('created_at', DateTime, default=func.now())
     description = Column(String(255), unique=False, nullable=False)
 
 
 class Price(Base):
-    __tablename__ = 'price'
+    __tablename__ = 'prices'
     id = Column(Integer, primary_key=True)
     product_id = Column('product_id', ForeignKey('product.id', ondelete='CASCADE'), default=None)
+    product = relationship("Product", back_populates="prices")
     weight = Column(String(20), unique=False, nullable=False)
     price = Column(Float, unique=False, nullable=False)
     old_price = Column(Float, unique=False, nullable=False)
@@ -77,3 +91,95 @@ class ProductCategory(Base):
     product = relationship("Product", back_populates="product_category")
     name = Column(String(100), unique=True, nullable=False)
     is_deleted = Column(Boolean, default=False)
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    product_id = Column('product_id', ForeignKey('product.id', ondelete='CASCADE'), default=None)
+    product = relationship("Product", back_populates="reviews")
+    user_id = Column('user_id', ForeignKey('user.id', ondelete='CASCADE'), default=None)
+    image_id = Column('image_id', ForeignKey('image.id', ondelete='CASCADE'), default=None)
+    rate = Column(Integer, default=5)
+    description = Column(String(255), unique=False, nullable=False)
+    created_at = Column('created_at', DateTime, default=func.now())
+    is_deleted = Column(Boolean, default=False)
+
+
+class Basket(Base):
+    __tablename__ = 'baskets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="basket")
+    basket_items = relationship("BasketItem", back_populates="basket")
+    order = relationship("BasketItem", uselist=False, back_populates="basket")
+
+
+class BasketItem(Base):
+    __tablename__ = 'basket_items'
+    id = Column(Integer, primary_key=True)
+    basket_id = Column(Integer, ForeignKey('basket.id'))
+    basket = relationship("BasketItem", back_populates="basket_items")
+    product_id = Column(Integer, ForeignKey('product.id'))
+    product = relationship("Product")
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="orders")
+    basket_id = Column(Integer, ForeignKey('basket.id'))
+    basket = relationship("Basket", back_populates="order")
+    price_order = Column(Float, unique=False, nullable=False)
+    payment_type = Column('payment_type', Enum(PaymentType), default=PaymentType.liqpay)
+    created_at = Column('created_at', DateTime, default=func.now())
+    confirmation_manager = Column(Boolean, default=False)
+    confirmation_pay = Column(Boolean, default=False)
+    call_manager = Column(Boolean, default=False)
+    #TODO status = maybe ENum...
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="posts")
+    nova_poshta_id = Column(Integer, ForeignKey('nova_poshta.id'))
+    nova_poshta = relationship("NovaPoshta", uselist=False, back_populates="post")
+    ukr_poshta_id = Column(Integer, ForeignKey('ukr_poshta.id'))
+    ukr_poshta = relationship("UkrPoshta", uselist=False, back_populates="post")
+
+
+class NovaPoshta(Base):
+    __tablename__ = 'nova_poshta'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", back_populates="nova_poshta")
+    address = Column(String(255), unique=False, nullable=True)
+
+
+class UkrPoshta(Base):
+    __tablename__ = 'ukr_poshta'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", back_populates="ukr_poshta")
+    address = Column(String(255), unique=False, nullable=True)
+
+
+class Favorite(Base):
+    __tablename__ = 'favorits'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="basket")
+    favorite_items_id = Column(Integer, ForeignKey('favorite_items.id'))
+    favorite_items = relationship("FavoriteItem", uselist=False, back_populates="favorite")
+
+
+class FavoriteItem(Base):
+    __tablename__ = 'favorite_items'
+    id = Column(Integer, primary_key=True)
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    favorite = relationship("FavoriteItem", back_populates="favorite_items")
+    product_id = Column(Integer, ForeignKey('product.id'))
+    product = relationship("Product")
