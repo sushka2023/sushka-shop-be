fef9244599c50dc5f0320ddb3ad14317d3e60610
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-07-02 01:58:30 +0300

    models and registration and login

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -52,8 +52,10 @@ async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
     user = await repository_users.get_user_by_email(body.username, db)
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
-    if not user.is_active:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is inactive")
+    if user.is_deleted:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is deleted")
+    if user.is_blocked:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is blocked")
     if not auth_service.pwd_context.verify(body.password, user.password_checksum):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
     # Generate JWT
