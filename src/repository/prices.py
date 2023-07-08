a20b6055f44276768615b165c309dcf70e5bc5b5
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-07-08 20:08:39 +0300

    get prices

--- src/repository/prices.py
+++ src/repository/prices.py
@@ -1,11 +1,14 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 from src.database.models import Price
 from src.schemas.price import PriceModel, PriceResponse
 
 
-async def price_by_product_id(body: int, db: Session) -> [PriceResponse]:
-    return db.query(Price).filter_by(product_id=body).all()
+async def price_by_product_id(id_product: int, db: Session) -> List[PriceResponse]:
+    price = db.query(Price).filter_by(product_id=id_product).all()
+    return price
 
 
 async def create_price(body: PriceModel, db: Session) -> PriceResponse:
--- src/routes/prices.py
+++ src/routes/prices.py
@@ -1,9 +1,12 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, status, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
 from src.database.models import Role
 from src.repository import prices as repository_prices
+from src.repository import products as repository_products
 from src.schemas.price import PriceResponse, PriceModel
 from src.services.roles import RoleAccess
 
@@ -18,6 +21,17 @@ allowed_operation_admin_moderator = RoleAccess([Role.admin, Role.moderator])
              response_model=PriceResponse,
              dependencies=[Depends(allowed_operation_admin_moderator)],
              status_code=status.HTTP_201_CREATED)
-async def create_category(body: PriceModel, db: Session = Depends(get_db)):
+async def create_price(body: PriceModel, db: Session = Depends(get_db)):
+    product = await repository_products.product_by_id(body.product_id, db)
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NOT_FOUND")
     new_price = await repository_prices.create_price(body, db)
     return new_price
+
+
+@router.get("/product_prices", response_model=List[PriceResponse])
+async def product_prices(id_product: int, db: Session = Depends(get_db)):
+    prod_prices = await repository_prices.price_by_product_id(id_product, db)
+    if prod_prices is None or len(prod_prices) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NOT_FOUND")
+    return prod_prices
