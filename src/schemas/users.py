a98b266a26115dcf1c6f8f5156d57edb5fc67217
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-07-13 20:34:13 +0300

    validate pass and email

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 from typing import Optional
+import re
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 from src.database.models import Role
+
+
+password_pattern = re.compile(r'^(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).+$')
+email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]{5,}@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
 
 class UserModel(BaseModel):
@@ -11,6 +16,18 @@ class UserModel(BaseModel):
     first_name: str = Field(min_length=3, max_length=150)
     last_name: str = Field(min_length=3, max_length=150)
     password_checksum: str = Field(min_length=8, max_length=255)
+
+    @validator('password_checksum', pre=True)
+    def validate_password(cls, password_checksum):
+        if not password_pattern.match(password_checksum):
+            raise ValueError('Password is not valid! The password must consist of at least one lowercase, uppercase letter, number and symbols.')
+        return password_checksum
+
+    @validator('email', pre=True)
+    def validate_email(cls, email):
+        if not email_pattern.match(email):
+            raise ValueError('Email is not valid! The email can consist of 5simbols@, Latin letters, numbers, dots and _')
+        return email
 
 
 class UserResponse(BaseModel):
