0a2cbcd30d97a7c6f3a7ce6b83e2e4b3ebc6650b
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-07-16 13:33:20 +0300

    add sort product by category

--- src/repository/products.py
+++ src/repository/products.py
@@ -3,7 +3,7 @@ from typing import List
 from sqlalchemy import desc, asc, nullslast
 from sqlalchemy.orm import Session
 
-from src.database.models import Product, Price
+from src.database.models import Product, Price, ProductCategory
 from src.schemas.product import ProductModel
 
 
@@ -15,20 +15,151 @@ async def product_by_id(body: int, db: Session) -> Product | None:
     return db.query(Product).filter_by(id=body).first()
 
 
-async def get_products(limit: int, offset: int, sort: str, db: Session) -> List[Product] | None:
-    products_ = None
-    if "id" in sort:
-        products_ = db.query(Product).filter(Product.is_deleted == False).order_by(asc(Product.id)).limit(limit).offset(offset).all()
-    elif "name" in sort:
-        products_ = db.query(Product).filter(Product.is_deleted == False).order_by(asc(Product.name)).limit(limit).offset(offset).all()
-    elif "low_price" in sort:
-        products_ = db.query(Product).join(Price).filter(Product.is_deleted == False).order_by(asc(Price.price)).limit(limit).offset(offset).all()
-    elif "haigh_price" in sort:
-        products_ = db.query(Product).join(Price).filter(Product.is_deleted == False).order_by(desc(Price.price)).limit(limit).offset(offset).all()
-    elif "low_date" in sort:
-        products_ = db.query(Product).filter(Product.is_deleted == False).order_by(asc(Product.created_at)).limit(limit).offset(offset).all()
-    elif "haigh_date" in sort:
-        products_ = db.query(Product).filter(Product.is_deleted == False).order_by(desc(Product.created_at)).limit(limit).offset(offset).all()
+# async def get_products(limit: int, offset: int, sort: str, db: Session) -> List[Product] | None:
+#     products_ = None
+#     if "id" in sort:
+#         products_ = db.query(Product).filter(Product.is_deleted == False).order_by(asc(Product.id)).limit(limit).offset(offset).all()
+#     elif "name" in sort:
+#         products_ = db.query(Product).filter(Product.is_deleted == False).order_by(asc(Product.name)).limit(limit).offset(offset).all()
+#     elif "low_price" in sort:
+#         products_ = db.query(Product).join(Price).filter(Product.is_deleted == False).order_by(asc(Price.price)).limit(limit).offset(offset).all()
+#     elif "haigh_price" in sort:
+#         products_ = db.query(Product).join(Price).filter(Product.is_deleted == False).order_by(desc(Price.price)).limit(limit).offset(offset).all()
+#     elif "low_date" in sort:
+#         products_ = db.query(Product).filter(Product.is_deleted == False).order_by(asc(Product.created_at)).limit(limit).offset(offset).all()
+#     elif "haigh_date" in sort:
+#         products_ = db.query(Product).filter(Product.is_deleted == False).order_by(desc(Product.created_at)).limit(limit).offset(offset).all()
+#     return products_
+
+
+async def get_products_id(limit: int, offset: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        filter(Product.is_deleted == False).\
+        order_by(asc(Product.id)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_id_by_category_id(limit: int, offset: int, category_id: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product). \
+        join(Product.product_category).\
+        filter(Product.is_deleted == False, ProductCategory.id == category_id).\
+        order_by(asc(Product.id)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_name(limit: int, offset: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        filter(Product.is_deleted == False).\
+        order_by(asc(Product.name)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    print(products_)
+    return products_
+
+
+async def get_products_name_by_category_id(limit: int, offset: int, category_id: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product). \
+        join(Product.product_category). \
+        filter(Product.is_deleted == False, ProductCategory.id == category_id).\
+        order_by(asc(Product.name)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_low_price(limit: int, offset: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        join(Price).\
+        filter(Product.is_deleted == False).\
+        order_by(asc(Price.price)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_low_price_by_category_id(limit: int, offset: int, category_id: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        join(Price). \
+        join(Product.product_category). \
+        filter(Product.is_deleted == False, ProductCategory.id == category_id).\
+        order_by(asc(Price.price)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_high_price(limit: int, offset: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        join(Price).\
+        filter(Product.is_deleted == False).\
+        order_by(desc(Price.price)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_high_price_by_category_id(limit: int, offset: int, category_id: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        join(Price). \
+        join(Product.product_category). \
+        filter(Product.is_deleted == False, ProductCategory.id == category_id).\
+        order_by(desc(Price.price)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_low_date(limit: int, offset: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        filter(Product.is_deleted == False).\
+        order_by(asc(Product.created_at)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_low_date_by_category_id(limit: int, offset: int, category_id: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product). \
+        join(Product.product_category). \
+        filter(Product.is_deleted == False, ProductCategory.id == category_id).\
+        order_by(asc(Product.created_at)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_high_date(limit: int, offset: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product).\
+        filter(Product.is_deleted == False).\
+        order_by(desc(Product.created_at)).\
+        limit(limit).\
+        offset(offset).\
+        all()
+    return products_
+
+
+async def get_products_high_date_by_category_id(limit: int, offset: int, category_id: int, db: Session) -> List[Product] | None:
+    products_ = db.query(Product). \
+        join(Product.product_category). \
+        filter(Product.is_deleted == False, ProductCategory.id == category_id).\
+        order_by(desc(Product.created_at)).\
+        limit(limit).\
+        offset(offset).\
+        all()
     return products_
 
 
