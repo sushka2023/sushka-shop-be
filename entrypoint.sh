aeb4944d1c2f103547ed11ab6cb7a85e808c7a4f
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-08-23 22:11:01 +0300

    add dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-poetry shell
-
-# Запускаємо Redis з паролем
+# Start Redis with password
 redis-server /etc/redis/redis.conf &
 
-# Запускаємо FastAPI
+# Activate virtual environment (optional if not needed)
+ poetry shell -n
+
+# Wait for Redis to fully start (adjust sleep time as needed)
+sleep 10
+
+# Start FastAPI application
 uvicorn main:app --host 0.0.0.0 --port 8000
