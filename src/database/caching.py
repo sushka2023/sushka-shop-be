1443ec39a97fe1484be9c8722dd44b449299d655
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-09-03 02:16:19 +0300

    redis bagfix

--- src/database/caching.py
+++ src/database/caching.py
@@ -9,17 +9,9 @@ def get_redis():
     redis_client = redis.Redis(
         host=settings.redis_host,
         port=settings.redis_port,
+        password=settings.redis_password,
         db=0
     )
-    # try:
-    #     redis_client.ping()  # Перевірка підключення
-    #     return redis_client
-    # except redis.exceptions.AuthenticationError as error:
-    #     print(f'Authentication failed to connect to Redis: {error}')
-    # except Exception as error:
-    #     print(f'Unable to connect to Redis: {error}')
-    # finally:
-    #     redis_client.connection_pool.disconnect()
     try:
         redis_client.ping()  # Check connection
         return redis_client  # Return the Redis client instance
