455e28dcc54e64c29e371e052d92ecf2700af725
Vladislav <109065019+tymoshchuk-vladyslav@users.noreply.github.com>
2023-09-24 15:49:23 +0300

    cloudinary

--- src/conf/config.py
+++ src/conf/config.py
@@ -3,21 +3,31 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str = 'postgresql+psycopg2://user:password@server:5432/database'
+
     secret_key: str = 'secret_key'
     algorithm: str = 'HS256'
+
     cloudinary_name: str = '_'
     cloudinary_api_key: str = '_'
     cloudinary_api_secret: str = '_'
+
     mail_username: str = 'username'
     mail_password: str = 'password'
     mail_from: str = 'from'
     mail_port: str = 'port'
     mail_server: str = 'server'
     mail_from_name: str = 'mail_name'
+
     redis_host: str = 'host_name'
     redis_port: str = 'port'
     redis_password: str = 'password'
-    sentry_url: str
+
+    sentry_url: str = 'url'
+
+    seed_email_adm: str = 'email'
+    seed_email_mod: str = 'email'
+    seed_email_use: str = 'email'
+    seed_password: str = 'password'
 
     class Config:
         env_file = ".env"
