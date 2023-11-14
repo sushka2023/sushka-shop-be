fe3b35be23a30de9d34c238f287b5b757ef7111b
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-11-14 23:30:57 +0200

    Added quantity field to basket items table

--- src/database/models.py
+++ src/database/models.py
@@ -186,7 +186,7 @@ class Basket(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship("User", back_populates="basket")
-    basket_items = relationship("BasketItem", uselist=False, back_populates="basket")
+    basket_items = relationship("BasketItem", uselist=True, back_populates="basket")
     order = relationship("Order", uselist=False, back_populates="basket")
 
 
@@ -197,6 +197,7 @@ class BasketItem(Base):
     basket = relationship("Basket", back_populates="basket_items")
     product_id = Column(Integer, ForeignKey('products.id'))
     product = relationship("Product")
+    quantity = Column(Integer, default=1)
 
 
 class Order(Base):
