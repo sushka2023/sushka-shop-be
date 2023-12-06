4aed195db69576ba87e5662d971d6bead96b7d1f
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-06 15:22:54 +0200

    Optimized services.cooperation.py

--- src/services/cooperation.py
+++ src/services/cooperation.py
@@ -1,25 +1,9 @@
-from pathlib import Path
-
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+from fastapi_mail import FastMail, MessageSchema, MessageType
 from pydantic import EmailStr
 from typing import Optional
 
 from src.conf.config import settings
-
-
-conf = ConnectionConfig(
-    MAIL_USERNAME=settings.mail_username,
-    MAIL_PASSWORD=settings.mail_password,
-    MAIL_FROM=settings.mail_from,
-    MAIL_PORT=settings.mail_port,
-    MAIL_SERVER=settings.mail_server,
-    MAIL_FROM_NAME=settings.mail_from_name,
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True,
-    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
-)
+from src.services.email import conf
 
 email_owner = settings.email_recipients
 
