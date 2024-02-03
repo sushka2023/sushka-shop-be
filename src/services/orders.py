2c5516cb090aa63f55d95a08f8b9e4b5315785a1
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-02-03 16:21:46 +0200

    Added function to calculate total cost of order, to move product from basket to order and to clear basket

--- src/services/orders.py
+++ src/services/orders.py
@@ -1,4 +1,12 @@
-from src.database.models import Basket
+import logging
+
+from fastapi import HTTPException, status
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+
+from src.database.models import Basket, BasketNumberAnonUser, BasketAnonUser, OrderedProduct
+
+logger = logging.getLogger(__name__)
 
 
 async def calculate_basket_total_cost(basket: Basket):
@@ -18,3 +26,53 @@ async def calculate_basket_total_cost(basket: Basket):
                 total_cost += float(price.price * basket_item.quantity)
 
     return total_cost
+
+
+async def calculate_basket_total_cost_for_anonym_user(basket: BasketNumberAnonUser):
+    """Determination of total order value for anonym user"""
+
+    print("Basket:", basket)
+    total_cost_order = 0.0
+
+    if basket.basket_items_anon_user:
+        for basket_item in basket.basket_items_anon_user:
+            print("Basket Item:", basket_item)
+            product = basket_item.product
+            price = next(
+                (p for p in product.prices if p.id == basket_item.price_id_by_anon_user),
+                None
+            )
+
+            if price:
+                total_cost_order += float(price.price * basket_item.quantity)
+
+    return total_cost_order
+
+
+async def delete_basket_items_by_basket_number_id(basket_number_id: int, db: Session):
+    """Anonym User Shopping Basket Cleaning"""
+    try:
+        db.query(BasketAnonUser).filter(BasketAnonUser.basket_number_id == basket_number_id).delete()
+        db.commit()
+    except SQLAlchemyError as e:
+        logger.exception("SQLAlchemyError")
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+async def move_product_to_ordered_for_anon_user(
+        db: Session, basket_item_anon_user: BasketAnonUser
+) -> OrderedProduct | None:
+    ordered_product = OrderedProduct(
+        product_id=basket_item_anon_user.product_id,
+        price_id=basket_item_anon_user.price_id_by_anon_user,
+        quantity=basket_item_anon_user.quantity
+    )
+    db.add(ordered_product)
+    db.commit()
+    db.refresh(ordered_product)
+
+    if ordered_product.id is not None:
+        return ordered_product
+    else:
+        return None
