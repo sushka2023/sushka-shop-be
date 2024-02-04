912bce95ab9ccb0d155a77c94717c7f2bcb7f3d8
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-02-04 23:17:15 +0200

    Added validate_phone_number and logging to cooperation router

--- src/routes/cooperation.py
+++ src/routes/cooperation.py
@@ -1,9 +1,13 @@
+import logging
+
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import JSONResponse
 
 from src.schemas.cooperation import CooperationModel
 from src.services.cooperation import email_service
+from src.services.validation import validate_phone_number
 
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/cooperation", tags=["cooperation"])
 
@@ -11,8 +15,13 @@ router = APIRouter(prefix="/cooperation", tags=["cooperation"])
 @router.post("/", response_class=JSONResponse)
 async def send_email(body: CooperationModel = Depends()):
     try:
-        await email_service.send_email(body.name, body.email, body.phone_number, body.message)
+        validate_phone_number(body.phone_number)
+        await email_service.send_email(**body.dict())
+    except ValueError as ve:
+        logger.error(f"Validation error: {str(ve)}")
+        return JSONResponse(content={"error": str(ve)}, status_code=422)
     except Exception as e:
+        logger.exception(f"An unexpected error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Failed to send email. Error: {str(e)}")
 
     return JSONResponse(content={"message": "Email sent successfully!"})
