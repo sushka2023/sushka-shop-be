d7d05cfdc3d455c4fb5ec420384ce8dfb379cf3a
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-02-11 10:08:49 +0200

    Removed unnecessary functions of anonym user and changed calculator

--- src/services/orders.py
+++ src/services/orders.py
@@ -4,9 +4,8 @@ from fastapi import HTTPException, status
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
-from src.database.models import (
-    Basket, BasketAnonUser, BasketItemAnonUser, OrderedProduct, BasketItem
-)
+from src.database.models import Basket, OrderedProduct, BasketItem
+
 
 logger = logging.getLogger(__name__)
 
@@ -30,21 +29,27 @@ async def calculate_basket_total_cost(basket: Basket):
     return total_cost
 
 
-async def calculate_basket_total_cost_for_anonym_user(basket: BasketAnonUser):
+async def get_items_by_order_id(order_id: int, db: Session):
+    return db.query(OrderedProduct).filter(OrderedProduct.order_id == order_id)
+
+
+async def calculate_basket_total_cost_for_anonym_user(order_id: int, db: Session):
     """Determination of total order value for anonym user"""
 
     total_cost_order = 0.0
 
-    if basket.basket_items_anon_user:
-        for basket_item in basket.basket_items_anon_user:
-            product = basket_item.product
+    order_items = await get_items_by_order_id(order_id=order_id, db=db)
+
+    if order_items:
+        for item in order_items:
+            product = item.products
             price = next(
-                (p for p in product.prices if p.id == basket_item.price_id_by_anon_user),
+                (p for p in product.prices if p.id == item.price_id),
                 None
             )
 
             if price:
-                total_cost_order += float(price.price * basket_item.quantity)
+                total_cost_order += float(price.price * item.quantity)
 
     return total_cost_order
 
@@ -65,39 +70,10 @@ async def move_product_to_ordered(db: Session, basket_item: BasketItem) -> Order
         return None
 
 
-async def move_product_to_ordered_for_anon_user(
-        db: Session, basket_item_anon_user: BasketItemAnonUser
-) -> OrderedProduct | None:
-    ordered_product = OrderedProduct(
-        product_id=basket_item_anon_user.product_id,
-        price_id=basket_item_anon_user.price_id_by_anon_user,
-        quantity=basket_item_anon_user.quantity
-    )
-    db.add(ordered_product)
-    db.commit()
-    db.refresh(ordered_product)
-
-    if ordered_product.id is not None:
-        return ordered_product
-    else:
-        return None
-
-
 async def delete_basket_items_by_basket_id(basket_id: int, db: Session):
     """User Shopping Cart Cleaning"""
     try:
         db.query(BasketItem).filter(BasketItem.basket_id == basket_id).delete()
-        db.commit()
-    except SQLAlchemyError as e:
-        logger.exception("SQLAlchemyError")
-        db.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
-
-
-async def delete_basket_items_by_basket_number_id(basket_anon_user_id: int, db: Session):
-    """Anonym User Shopping Basket Cleaning"""
-    try:
-        db.query(BasketItemAnonUser).filter(BasketItemAnonUser.basket_anon_user_id == basket_anon_user_id).delete()
         db.commit()
     except SQLAlchemyError as e:
         logger.exception("SQLAlchemyError")
