25e19164878ee1843b552ef050bf28b6a87a46f2
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-02-27 17:53:50 +0200

    Realised a possible to search product by name or by id

--- src/routes/products.py
+++ src/routes/products.py
@@ -1,7 +1,8 @@
 import pickle
-from typing import List
+from typing import Union
 
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException, Query
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -300,3 +301,49 @@ async def get_one_product(product_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=Ex.HTTP_404_NOT_FOUND)
 
     return product
+
+
+@router.get(
+    "/search_for_crm/",
+    dependencies=[Depends(allowed_operation_admin_moderator)],
+    response_model=ProductWithTotalResponse
+)
+async def search_all_products_for_crm(
+        limit: int,
+        offset: int,
+        search_query: Union[int, str] = Query(..., min_length=3),
+        db: Session = Depends(get_db)
+):
+    """
+    The search_all_products_for_crm function returns a list of products for the CRM after search.
+
+        :param limit: int: Limit the number of products returned
+        :param offset: int: Indicate the number of records to skip
+        :param search_query: product search criterion (by name or id of the product)
+        :param db: Session: Pass the database connection to the function
+
+    Return: A list of products
+    """
+
+    return await repository_products.search_products_for_crm(search_query, db, offset, limit)
+
+
+@router.get("/search/", response_model=ProductWithTotalResponse)
+async def search_all_products(
+        limit: int,
+        offset: int,
+        search_query: Union[int, str] = Query(..., min_length=3),
+        db: Session = Depends(get_db)
+):
+    """
+    The search_all_products function returns a list of products after search.
+
+        :param limit: int: Limit the number of products returned
+        :param offset: int: Indicate the number of records to skip
+        :param search_query: product search criterion (by name or id of the product)
+        :param db: Session: Pass the database connection to the function
+
+    Return: A list of products
+    """
+
+    return await repository_products.search_all_products(search_query, db, offset, limit)
