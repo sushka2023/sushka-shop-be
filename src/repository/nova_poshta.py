628093d07d9d3f73841e48e2555acf04f8a75bd7
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-04 19:42:05 +0300

    Added function to obtain data from API Nova Poshta, update these data and delete from db

--- src/repository/nova_poshta.py
+++ src/repository/nova_poshta.py
@@ -1,8 +1,132 @@
+import httpx
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
+from src.conf.config import settings
 from src.database.models import NovaPoshta, post_novaposhta_association, Post, User
 from src.services.exception_detail import ExDetail as Ex
+
+API_KEY = settings.api_key_nova_poshta
+API_URL = settings.api_url_nova_poshta
+
+
+async def create_nova_poshta_warehouse(
+    city: str, address_warehouse: str, category_warehouse: str, area: str, region: str, db: Session,
+) -> NovaPoshta:
+    db_warehouse = NovaPoshta(
+        city=city,
+        address_warehouse=address_warehouse,
+        category_warehouse=category_warehouse,
+        area=area,
+        region=region,
+    )
+
+    db.add(db_warehouse)
+    db.commit()
+    db.refresh(db_warehouse)
+
+    return db_warehouse
+
+
+async def get_all_warehouses(db: Session) -> list[NovaPoshta]:
+    return db.query(NovaPoshta).all()
+
+
+async def get_warehouses_data_for_specific_city(db: Session, city_name: str) -> list[str]:
+    existing_warehouses = db.query(NovaPoshta).filter_by(city=city_name).all()
+    existing_addresses = {warehouse.address_warehouse for warehouse in existing_warehouses}
+
+    url = f"{API_URL}"
+    payload = {
+        "apiKey": API_KEY,
+        "modelName": "Address",
+        "calledMethod": "getWarehouses",
+        "methodProperties": {
+            "CityName": city_name
+        },
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url=url, json=payload)
+
+    response.raise_for_status()
+    data = response.json()
+
+    if not data.get("success"):
+        print(f"Error: Request for city {city_name} was not successful.")
+        return sorted(list(existing_addresses))
+
+    warehouses_data = data.get("data", [])
+
+    for item in warehouses_data:
+        address_warehouse = item.get("Description", "")
+        if address_warehouse not in existing_addresses:
+            db_warehouse = await create_nova_poshta_warehouse(
+                city=item.get("CityDescription", ""),
+                address_warehouse=address_warehouse,
+                category_warehouse=item.get("CategoryOfWarehouse", ""),
+                area=item.get("SettlementAreaDescription", ""),
+                region=item.get("SettlementRegionsDescription", ""),
+                db=db
+            )
+
+            existing_addresses.add(db_warehouse.address_warehouse)
+
+    all_warehouses = db.query(NovaPoshta).filter_by(city=city_name).order_by(NovaPoshta.id).all()
+
+    return [warehouse.address_warehouse for warehouse in all_warehouses]
+
+
+async def update_warehouses_data(db: Session) -> None:
+    existing_cities = db.query(NovaPoshta.city).distinct().all()
+    existing_cities = {city[0] for city in existing_cities}
+
+    url = f"{API_URL}"
+    payload = {
+        "apiKey": API_KEY,
+        "modelName": "Address",
+        "calledMethod": "getWarehouses",
+        "methodProperties": {
+            "Limit": "",
+            "Page": ""
+        },
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url=url, json=payload)
+
+    response.raise_for_status()
+    data = response.json()
+
+    if not data.get("success"):
+        print("Error: Request was not successful.")
+        return
+
+    warehouses_data = data.get("data", [])
+
+    for item in warehouses_data:
+        city = item.get("CityDescription", "")
+        if city in existing_cities:
+            update_warehouse = db.query(NovaPoshta).filter_by(address_warehouse=item.get("Description", "")).first()
+
+            if update_warehouse:
+                update_warehouse.category_warehouse = item.get("CategoryOfWarehouse", "")
+                update_warehouse.area = item.get("SettlementAreaDescription", "")
+                update_warehouse.region = item.get("SettlementRegionsDescription", "")
+            else:
+                await create_nova_poshta_warehouse(
+                    db=db,
+                    city=city,
+                    address_warehouse=item.get("Description", ""),
+                    category_warehouse=item.get("CategoryOfWarehouse", ""),
+                    area=item.get("SettlementAreaDescription", ""),
+                    region=item.get("SettlementRegionsDescription", "")
+                )
+    db.commit()
+
+
+async def delete_all_warehouses(db: Session) -> None:
+    db.query(NovaPoshta).delete()
+    db.commit()
 
 
 async def get_nova_poshta_by_id(nova_poshta_id: int, db: Session) -> NovaPoshta | None:
--- src/routes/nova_poshta.py
+++ src/routes/nova_poshta.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -7,6 +7,7 @@ from src.repository import nova_poshta as repository_novaposhta
 from src.schemas.nova_poshta import (
     NovaPoshtaAddressDeliveryResponse,
     NovaPoshtaAddressDeliveryPartialUpdate,
+    NovaPoshtaMessageResponse,
 )
 from src.services.cache_in_redis import delete_cache_in_redis
 from src.services.roles import RoleAccess
@@ -18,6 +19,29 @@ router = APIRouter(prefix="/nova_poshta", tags=["novaposhta offices"])
 allowed_operation_admin = RoleAccess([Role.admin])
 allowed_operation_admin_moderator = RoleAccess([Role.admin, Role.moderator])
 allowed_operation_admin_moderator_user = RoleAccess([Role.admin, Role.moderator, Role.user])
+
+
+@router.get("/warehouses/{city}", response_model=list[str])
+async def get_warehouses_for_city(city: str, db: Session = Depends(get_db)) -> list[str]:
+    return await repository_novaposhta.get_warehouses_data_for_specific_city(db=db, city_name=city)
+
+
+@router.put("/update_warehouses", response_model=NovaPoshtaMessageResponse)
+async def update_warehouses_data(db: Session = Depends(get_db)) -> dict[str, str]:
+    warehouses = await repository_novaposhta.get_all_warehouses(db=db)
+    if not warehouses:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Warehouses do not exist in the database"
+        )
+
+    await repository_novaposhta.update_warehouses_data(db=db)
+
+    return {"message": "Warehouses data updated successfully."}
+
+
+@router.delete("/delete_warehouses", status_code=status.HTTP_204_NO_CONTENT)
+async def remove_warehouses_data(db: Session = Depends(get_db)) -> None:
+    await repository_novaposhta.delete_all_warehouses(db=db)
 
 
 @router.patch("/{nova_poshta_id}/partial-update",
